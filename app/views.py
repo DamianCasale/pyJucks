debc45cb03a4f943ad0e74b916a63ac96013410f
Damian Casale <damian.casale@gmail.com>
2014-12-11 12:16:35 +0000

    removed narcissism

--- app/views.py
+++ app/views.py
@@ -5,7 +5,6 @@ from app import app
 def index():
 	return render_template("index.html",
                            title='Playing with Jinja & Nunjucks',
-                           user={ 'name':'Damian'},
                            renderedBy='Flask and Jinja2',
                            anEvent='Normal backend functionality',
                            aCounter=0)
