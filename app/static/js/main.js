320efdb48bb7ccd521874c1c4ea3898bbeba74c2
Damian Casale <damian.casale@gmail.com>
2014-12-11 15:02:32 +0000

    if xhr else, jquery, data : dict>json

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -5,10 +5,27 @@ var toReplace	= document.getElementById('toReplace');
 
 var nunEnv		= nunjucks.configure('static/partials');
 
+
 theButton.addEventListener('click', function() {
 	var rendered		= nunEnv.render('first.html', { 'renderedBy':'Nunjucks',
 														'anEvent':' Replaced in browser', 
 														'aCounter':theCounter });
 	toReplace.innerHTML	= rendered;
 	theCounter++;
+});
+
+
+
+
+
+
+
+$('.ajaxableLink').on('click', function(e){
+	e.preventDefault();
+	alert("Ajaxable link clicked : " + this.href);
+
+	$.get(this.href, function( data ){
+		alert( "The ajax request returned : " + data );
+	});
+
 });
--- app/views.py
+++ app/views.py
@@ -1,25 +1,54 @@
+import json
+
 from flask import render_template, request
 from app import app
 
 @app.route('/')
 def index():
-	return render_template("index.html",
-                           title='Playing with Jinja & Nunjucks',
-                           renderedBy='Flask and Jinja2',
-                           anEvent='Normal backend functionality',
-                           aCounter=0)
+
+
+	pageInfo = {
+				'template': 'index.html',
+				'replaces': 'theMainContent',
+				'data' : {
+					'title':		'Playing with Jinja & Nunjucks',
+					'renderedBy':	'Flask and Jinja2',
+					'anEvent':		'Normal backend functionality',
+					'aCounter':		0}}
+
+	if request.is_xhr:
+		return json.dumps(pageInfo)
+
+	else:
+		return render_template( pageInfo['template'], 
+								title		= pageInfo['data']['title'],
+								renderedBy	= pageInfo['data']['renderedBy'],
+								anEvent		= pageInfo['data']['anEvent'],
+								aCounter	= pageInfo['data']['aCounter'])
 
 @app.route('/page1')
 def page1():
 
+
+	pageInfo = {
+			'template':	'page1.html',
+		    'replaces':	'theMainContent',
+		    'data': {
+					'title':		'Playing with Jinja & Nunjucks',
+					'renderedBy':	'Flask and Jinja2',
+					'anEvent':		'Normal backend functionality',
+					'aCounter':		0}}
+
+
 	if request.is_xhr: 
 		print "XHR REQUEST"
 		#here we should send back only the json describing the page to be rendered via Nunjucks
+		return json.dumps(pageInfo)
 	else: 
 		print "NON XHR"
 		#here should be the normal page render
-		return render_template("page1.html",
-		                   title='Playing with Jinja & Nunjucks',
-                           renderedBy='Flask and Jinja2',
-                           anEvent='Normal backend functionality',
-                           aCounter=0)
+		return render_template( pageInfo['template'], 
+								title		= pageInfo['data']['title'],
+								renderedBy	= pageInfo['data']['renderedBy'],
+								anEvent		= pageInfo['data']['anEvent'],
+								aCounter	= pageInfo['data']['aCounter'])
