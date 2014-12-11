36ddce98a80ebd134382bb4e365943b397610d81
Damian Casale <damian.casale@gmail.com>
2014-12-11 12:08:28 +0000

    text change

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -6,8 +6,8 @@ var toReplace	= document.getElementById('toReplace');
 var nunEnv		= nunjucks.configure('static/partials');
 
 theButton.addEventListener('click', function() {
-	var rendered		= nunEnv.render('first.html', { 'renderedBy':'Nunjucks in browser!',
-														'anEvent':' Replaced on in browser', 
+	var rendered		= nunEnv.render('first.html', { 'renderedBy':'Nunjucks',
+														'anEvent':' Replaced in browser', 
 														'aCounter':theCounter });
 	toReplace.innerHTML	= rendered;
 	theCounter++;
