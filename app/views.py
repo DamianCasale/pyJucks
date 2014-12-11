cd71d49c9ab7f7215e5ef2857df7afb4ae331b21
Damian Casale <damian.casale@gmail.com>
2014-12-11 12:50:09 +0000

    STASH : starting to distinguish between xhr and normal requestsd

--- app/views.py
+++ app/views.py
@@ -1,10 +1,25 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 @app.route('/')
 def index():
 	return render_template("index.html",
                            title='Playing with Jinja & Nunjucks',
+                           renderedBy='Flask and Jinja2',
+                           anEvent='Normal backend functionality',
+                           aCounter=0)
+
+@app.route('/page1')
+def page1():
+
+	if request.is_xhr: 
+		print "XHR REQUEST"
+		#here we should send back only the json describing the page to be rendered via Nunjucks
+	else: 
+		print "NON XHR"
+		#here should be the normal page render
+		return render_template("page1.html",
+		                   title='Playing with Jinja & Nunjucks',
                            renderedBy='Flask and Jinja2',
                            anEvent='Normal backend functionality',
                            aCounter=0)
